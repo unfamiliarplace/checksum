348259d05fc4f8828bd54c645c2188b7a221f2c5
Luke Sawczak <luke@sawczak.com>
2026-04-09 22:52:38 -0400

    test

---
+++ src/main.py
@@ -0,0 +1,81 @@
+import itertools
+
+"""
+OPERATIONS
+A = add
+a = substract
+M = multiply
+m = multiply by negative
+D = divide
+d = divide by negative
+"""
+
+MULT_FACTOR = 1/10
+DIV_FACTOR = 1/10
+
+def op_A(c: str) -> int:
+    return lambda base: base + ord(c)
+
+def op_a(c: str) -> int:
+    return lambda base: base - ord(c)
+
+def op_M(c: str) -> int:
+    return lambda base: round(base * (MULT_FACTOR * ord(c)))
+
+def op_m(c: str) -> int:
+    return lambda base: -1 * round(base * (MULT_FACTOR * ord(c)))
+
+def op_D(c: str) -> int:
+    return lambda base: round(base / (DIV_FACTOR * ord(c)))
+
+def op_d(c: str) -> int:
+    return lambda base: -1 * round(base / (DIV_FACTOR * ord(c)))
+
+SEQ_BASE = 'AMDamd'
+
+OPS = {
+    'A': op_A,
+    'a': op_a,
+    'M': op_M,
+    'm': op_m,
+    'D': op_D,
+    'd': op_d
+}
+
+SEQ = ''
+
+def make_sequence():
+    global SEQ
+    SEQ = ''
+    for p in itertools.permutations(SEQ_BASE):
+        SEQ += ''.join(p)
+
+def make_sum(s: str) -> int:
+    base = 1
+    for i_c in range(len(s)):
+        i_o = i_c % len(OPS)
+        op = OPS[SEQ[i_o]](s[i_c])
+        base = op(base)
+    return base
+
+def check_sum(s: str, check: int) -> bool:
+    return make_sum(s) == check
+
+def check_sums(s1: str, s2: str) -> bool:
+    return check_sum(s1, make_sum(s2))
+
+def test():
+    for word in 'this is a test sith si a sett'.split():
+        print(f'{word:<5} {make_sum(word)}')
+    
+    test1 = 'Extraordinarily long phrase!'
+    test2 = 'ndl!tyaEhsliraoeixrr nrao pg'
+    print(test1, make_sum(test1))
+    print(test2, make_sum(test2))
+
+def run():
+    make_sequence()
+    test()
+
+if __name__ == '__main__':
+    run()
